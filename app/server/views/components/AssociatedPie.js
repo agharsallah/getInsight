b17f3c0a6dfc4cf0abdea89c96ab53daab8ee84b
abdou <aa.test20@gmail.com>
2016-10-21 01:07:28 +0100

    implemented hoverable pie
    needs optimization

--- app/server/views/components/AssociatedPie.js
+++ app/server/views/components/AssociatedPie.js
@@ -1,26 +1,70 @@
 import React,{Component} from 'react';
 import  { PieChart, Pie, Sector, Cell } from 'Recharts'
 
-
 class AssociatedPie extends Component{
+	constructor(props) {
+		super(props);
+		this.state={
+			activeIndex:0
+		}
+		this.onPieEnter=this.onPieEnter.bind(this)
+	}
+	onPieEnter(data, index) {
+		console.log(index)
+		this.setState({
+			activeIndex: index
+		});
 
+  	}
 	render(){
-const data = [{name: 'male', value: 5557968}, {name: 'female', value: 5596404}];
-const COLORS = ['#0088FE',  '#FF8042'];
+		const data = [{name: 'Group A', value: 400}, {name: 'Group B', value: 300},
+                  {name: 'Group C', value: 300}, {name: 'Group D', value: 200}];
+        const COLORS = ['#0088FE',  '#FF8042'];
+const renderActiveShape = (props) => {
 
-const RADIAN = Math.PI / 180;                    
-const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent, index }) => {
- 	const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
-  const x  = cx + radius * Math.cos(-midAngle * RADIAN);
-  const y = cy  + radius * Math.sin(-midAngle * RADIAN);
- 
+	 const RADIAN = Math.PI / 180;
+  const { cx, cy, midAngle, innerRadius, outerRadius, startAngle, endAngle,
+    fill, payload, percent, value } = props;
+  const sin = Math.sin(-RADIAN * midAngle);
+  const cos = Math.cos(-RADIAN * midAngle);
+  const sx = cx + (outerRadius + 10) * cos;
+  const sy = cy + (outerRadius + 10) * sin;
+  const mx = cx + (outerRadius + 30) * cos;
+  const my = cy + (outerRadius + 30) * sin;
+  const ex = mx + (cos >= 0 ? 1 : -1) * 22;
+  const ey = my;
+  const textAnchor = cos >= 0 ? 'start' : 'end';
+
   return (
-    <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} 	dominantBaseline="central">
-    	{`${(percent * 100).toFixed(0)}%`}
-    </text>
-
+    <g>
+      <text x={cx} y={cy} dy={8} textAnchor="middle" fill={fill}>{payload.name}</text>
+      <Sector
+        cx={cx}
+        cy={cy}
+        innerRadius={innerRadius}
+        outerRadius={outerRadius}
+        startAngle={startAngle}
+        endAngle={endAngle}
+        fill={fill}
+      />
+      <Sector
+        cx={cx}
+        cy={cy}
+        startAngle={startAngle}
+        endAngle={endAngle}
+        innerRadius={outerRadius + 6}
+        outerRadius={outerRadius + 10}
+        fill={fill}
+      />
+      <path d={`M${sx},${sy}L${mx},${my}L${ex},${ey}`} stroke={fill} fill="none"/>
+      <circle cx={ex} cy={ey} r={2} fill={fill} stroke="none"/>
+      <text x={ex + (cos >= 0 ? 1 : -1) * 12} y={ey} textAnchor={textAnchor} fill="#333">{`PV ${value}`}</text>
+      <text x={ex + (cos >= 0 ? 1 : -1) * 12} y={ey} dy={18} textAnchor={textAnchor} fill="#999">
+        {`(Rate ${(percent * 100).toFixed(2)}%)`}
+      </text>
+    </g>
   );
-};
+}
 		return(
 			<div>
 			<PieChart width={800} height={400}  onMouseEnter={this.onPieEnter}>
@@ -29,18 +73,24 @@ const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, per
           cx={400} 
           cy={100} 
           labelLine={false}
-          label={renderCustomizedLabel}
+          innerRadius={70}
           outerRadius={90} 
           fill="#8884d8"
+          activeIndex={this.state.activeIndex}
+          activeShape={renderActiveShape}
+          isAnimationActive={false}
         >
         	{
           	data.map((entry, index) => <Cell fill={COLORS[index % COLORS.length]}/>)
-          }
+          	}
         </Pie>
       </PieChart>
 			</div>
-		)
-	}
+		);
+	
+};
+
+
 };
 export default AssociatedPie
 
